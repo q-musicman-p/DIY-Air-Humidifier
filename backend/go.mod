a70bac9776c358d7d50f7edeecfb963bd9dc21c4
Artem Ermilov <artem.ermilov@rfdyn.ru>
2026-09-13 21:42:12 +0300

    NEW: db: add influx data base

--- backend/go.mod
+++ backend/go.mod
@@ -5,9 +5,14 @@ go 1.26
 require (
 	github.com/eclipse/paho.mqtt.golang v1.5.1
 	github.com/gorilla/websocket v1.5.3
+	github.com/influxdata/influxdb-client-go/v2 v2.14.0
 )
 
 require (
+	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/influxdata/line-protocol v0.0.0-20200327222509-2487e7298839 // indirect
+	github.com/oapi-codegen/runtime v1.0.0 // indirect
 	golang.org/x/net v0.44.0 // indirect
 	golang.org/x/sync v0.17.0 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"errors"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/gorilla/websocket"
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/influxdata/influxdb-client-go/v2/api"
 )
 
 type Telemetry struct {
@@ -28,24 +31,44 @@ var (
 		},
 	}
 	mqqtInbound = make(chan Telemetry)
+	influxChan  = make(chan Telemetry)
 	clientReg   = make(chan WebClient)
 	clientUnreg = make(chan chan Telemetry)
 )
 
+func influxWriter(writeAPI api.WriteAPI) {
+	for data := range influxChan {
+		writeAPI.WritePoint(
+			influxdb2.NewPointWithMeasurement("telemetry").
+			AddField("temperature", data.Temperature).
+			AddField("humidity", data.Humidity))
+	}
+
+	writeAPI.Flush()
+}
+
 func stateDispatcher() {
 	activeClients := make(map[chan Telemetry]bool)
-
 	var latestData Telemetry
 
-	for {
+MainLoop:
+	for {  
 		select {
-		case data := <-mqqtInbound:
+		case data, ok := <-mqqtInbound:
+			if !ok {
+				break MainLoop
+			}
+
 			latestData = data
 			for clientCh := range activeClients {
 				select {
 				case clientCh <- data:
 				default:
 				}
+			}
+			select {
+			case influxChan <- data:
+			default:
 			}
 		case client := <-clientReg:
 			activeClients[client.ch] = true
@@ -54,6 +77,12 @@ func stateDispatcher() {
 			delete(activeClients, clientCh)
 			close(clientCh)
 		}
+	}
+
+	log.Printf("Dispatcher was stopped")
+	close(influxChan)
+	for clientCh := range activeClients {
+		close(clientCh)
 	}
 }
 
@@ -95,10 +124,28 @@ func telemetryConnection(w http.ResponseWriter, r *http.Request)  {
 }
 
 func main() {
+	mqttURL := os.Getenv("MQTT_BROKER_URL")
+	influxURL := os.Getenv("INFLUXDB_URL")
+	influxToken := os.Getenv("INFLUXDB_TOKEN")
+	influxOrg := os.Getenv("INFLUXDB_ORG")
+	influxBucket := os.Getenv("INFLUXDB_BUCKET")
+
 	go stateDispatcher()
 
+	influxClient := influxdb2.NewClient(influxURL, influxToken)
+	defer influxClient.Close()
+
+	writeAPI := influxClient.WriteAPI(influxOrg, influxBucket)
+	errorsCh := writeAPI.Errors()
+	go func() {
+		for err := range errorsCh {
+			log.Printf("Error while writing to InfluxDB: %v\n", err)
+		}
+	}()
+	go influxWriter(writeAPI)
+
 	opts := mqtt.NewClientOptions()
-	opts.AddBroker(os.Getenv("MQTT_BROKER_URL"))
+	opts.AddBroker(mqttURL)
 	opts.SetClientID("go_backend_client")
 	opts.SetDefaultPublishHandler(messagePubHandler)
 
@@ -114,9 +161,17 @@ func main() {
 	log.Printf("Successfully subscribed on topic: %s\n", topic)
 
 	http.HandleFunc("/telemetry", telemetryConnection)
+	go func () {
+		if err := http.ListenAndServe(":8080", nil); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatal(err)
+		}
+	}()
 
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		panic(err)
-	}
+	stopSignal := make(chan os.Signal, 1)
+	signal.Notify(stopSignal, os.Interrupt)
+	<-stopSignal
+
+	mqttClient.Disconnect(250)
+	close(mqqtInbound)
+
 }
